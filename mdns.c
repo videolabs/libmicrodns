4367180683159d4a235681f230ca3af489ce4faa
joseph calderon <calderon.joe@gmail.com>
2015-12-27 12:08:56 -0800

    refactor send() and recv() to be more generic

--- mdns.c
+++ mdns.c
@@ -31,18 +31,8 @@ struct mdns_ctx {
         struct sockaddr_storage addr;
 };
 
-struct mdns_hdr {
-        uint16_t id;
-        uint16_t flags;
-        uint16_t num_qn;
-        uint16_t num_ans_rr;
-        uint16_t num_auth_rr;
-        uint16_t num_add_rr;
-};
-
 static int mdns_resolve(struct sockaddr_storage *, const char *, unsigned short);
-static ssize_t mdns_write(uint8_t *, const struct mdns_hdr *, const struct rr_entry *);
-static struct rr_entry *mdns_read(const uint8_t *, size_t);
+static ssize_t mdns_write_hdr(uint8_t *, const struct mdns_hdr *);
 static int strrcmp(const char *, const char *);
 
 static int
@@ -144,9 +134,9 @@ mdns_cleanup(struct mdns_ctx *ctx)
 }
 
 static ssize_t
-mdns_write(uint8_t *ptr, const struct mdns_hdr *hdr, const struct rr_entry *entry)
+mdns_write_hdr(uint8_t *ptr, const struct mdns_hdr *hdr)
 {
-        uint8_t *name, *p = ptr;
+        uint8_t *p = ptr;
 
         p = write_u16(p, hdr->id);
         p = write_u16(p, hdr->flags);
@@ -154,45 +144,34 @@ mdns_write(uint8_t *ptr, const struct mdns_hdr *hdr, const struct rr_entry *entr
         p = write_u16(p, hdr->num_ans_rr);
         p = write_u16(p, hdr->num_auth_rr);
         p = write_u16(p, hdr->num_add_rr);
-        if ((name = rr_encode(entry->name)) == NULL)
-                return (MDNS_STDERR);
-        p = write_raw(p, name);
-        p = write_u16(p, entry->type);
-        p = write_u16(p, (entry->rr_class & ~0x8000) | (entry->msbit << 15));
-
-        free(name);
         return (p - ptr);
 }
 
 int
-mdns_send(const struct mdns_ctx *ctx, enum rr_type type, const char *name)
+mdns_send(const struct mdns_ctx *ctx, const struct mdns_hdr *hdr, const struct rr_entry *entries)
 {
-        struct mdns_hdr hdr;
-        struct rr_entry entry;
-        uint8_t buf[MDNS_PKT_MAXSZ];
-        ssize_t n, r;
+        uint8_t buf[MDNS_PKT_MAXSZ] = {0};
+        const struct rr_entry *entry = entries;
+        ssize_t i, n = 0, l, r;
 
-        if (strlen(name) >= MDNS_DN_MAXSZ) {
-                errno = EINVAL;
+        if (!entries) return (MDNS_STDERR);
+
+        if ((l = mdns_write_hdr(buf, hdr)) < 0) {
                 return (MDNS_STDERR);
         }
+        n += l;
 
-        memset(&hdr, 0, sizeof(hdr));
-        hdr.num_qn = 1;
-        entry.type = type;
-        entry.rr_class = RR_IN;
-        entry.msbit = 0; // ask for multicast responses
-        if((entry.name = strdup(name)) == NULL)
-                return (MDNS_STDERR);
-
-        if ((n = mdns_write(buf, &hdr, &entry)) < 0) {
-                free(entry.name);
-                return (MDNS_STDERR);
+        for (entry = entries; entry; entry = entry->next) {
+        l = rr_write(buf+n, entry, (hdr->flags & FLAG_QR) > 0);
+                if (l < 0) {
+                        return (MDNS_STDERR);
+                }
+                n += l;
         }
+
         r = sendto(ctx->sock, (const char *) buf, n, 0,
-            (const struct sockaddr *) &ctx->addr, ss_len(&ctx->addr));
+                (const struct sockaddr *) &ctx->addr, ss_len(&ctx->addr));
 
-        free(entry.name);
         return (r < 0 ? MDNS_NETERR : 0);
 }
 
@@ -208,63 +187,56 @@ mdns_free(struct rr_entry *entries)
         }
 }
 
-static struct rr_entry *
-mdns_read(const uint8_t *ptr, size_t n)
+static const uint8_t *
+mdns_read_header(const uint8_t *ptr, size_t n, struct mdns_hdr *hdr)
 {
-        const uint8_t *root = ptr;
-        struct mdns_hdr hdr;
-        struct rr_entry *entry, *entries = NULL;
-        int num_entry;
-
-        if (n <= sizeof(hdr)) {
+        if (n <= sizeof(struct mdns_hdr)) {
                 errno = ENOSPC;
-                return (NULL);
+                return NULL;
         }
-        memcpy(&hdr, ptr, sizeof(hdr));
-        ptr += sizeof(hdr);
-        n -= sizeof(hdr);
+        ptr = read_u16(ptr, &n, &hdr->id);
+        ptr = read_u16(ptr, &n, &hdr->flags);
+        ptr = read_u16(ptr, &n, &hdr->num_qn);
+        ptr = read_u16(ptr, &n, &hdr->num_ans_rr);
+        ptr = read_u16(ptr, &n, &hdr->num_auth_rr);
+        ptr = read_u16(ptr, &n, &hdr->num_add_rr);
+        return ptr;
+}
 
-        if (ntohs(hdr.num_qn) > 0) {
-                errno = ENOTSUP; // XXX support only answers
-                return (NULL);
-        }
-        num_entry = ntohs(hdr.num_ans_rr) + ntohs(hdr.num_add_rr);
+int
+mdns_recv(const struct mdns_ctx *ctx, struct mdns_hdr *hdr, struct rr_entry **entries)
+{
+        uint8_t buf[MDNS_PKT_MAXSZ];
+        ssize_t n, num_entry;
+        struct rr_entry *entry;
+
+        *entries = NULL;
+again:  
+        if ((n = recv(ctx->sock, (char *) buf, sizeof(buf), 0)) < 0)
+                return (MDNS_NETERR);
+
+        const uint8_t *ptr = mdns_read_header(buf, n, hdr);
+
+        num_entry = hdr->num_qn + hdr->num_ans_rr + hdr->num_add_rr;
         for (int i = 0; i < num_entry; ++i) {
                 entry = calloc(1, sizeof(struct rr_entry));
                 if (!entry)
                         goto err;
-                ptr = rr_read(ptr, &n, root, entry, 1);
+                ptr = rr_read(ptr, &n, buf, entry, (hdr->flags & FLAG_QR) > 0);
                 if (!ptr) {
                         errno = ENOSPC;
                         goto err;
                 }
-                entry->next = entries;
-                entries = entry;
+                entry->next = *entries;
+                *entries = entry;
         }
-        return (entries);
-err:
-        mdns_free(entries);
-        return (NULL);
-}
-
-int
-mdns_recv(const struct mdns_ctx *ctx, struct rr_entry **entries)
-{
-        uint8_t buf[MDNS_PKT_MAXSZ];
-        ssize_t n;
-
-        *entries = NULL;
-again:
-        if ((n = recv(ctx->sock, (char *) buf, sizeof(buf), 0)) < 0)
-                return (MDNS_NETERR);
-
-        *entries = mdns_read(buf, n);
         if (*entries == NULL) {
-                if (errno == ENOTSUP)
-                        goto again;
                 return (MDNS_STDERR);
         }
         return (0);
+err:
+        mdns_free(*entries);
+        return (MDNS_STDERR);
 }
 
 void
@@ -301,31 +273,42 @@ strrcmp(const char *s1, const char *s2)
 }
 
 int
-mdns_listen(const struct mdns_ctx *ctx, const char *name, unsigned int interval,
+mdns_listen(const struct mdns_ctx *ctx, const char *name, enum rr_type type, unsigned int interval,
     mdns_stop_func stop, mdns_callback callback, void *p_cookie)
 {
         int r;
         time_t t1, t2;
+        struct mdns_hdr hdr = {0};
+        struct rr_entry qn = {0};
+
+        hdr.num_qn  = 1;
+        qn.name     = (char *)name;
+        qn.type     = type;
+        qn.rr_class = RR_IN;
 
         if (setsockopt(ctx->sock, SOL_SOCKET, SO_RCVTIMEO, (const void *) &os_deadline, sizeof(os_deadline)) < 0)
                 return (MDNS_NETERR);
         if (setsockopt(ctx->sock, SOL_SOCKET, SO_SNDTIMEO, (const void *) &os_deadline, sizeof(os_deadline)) < 0)
                 return (MDNS_NETERR);
 
-        if ((r = mdns_send(ctx, RR_PTR, name)) < 0) // send a first probe request
+        if ((r = mdns_send(ctx, &hdr, &qn)) < 0) // send a first probe request
                 callback(p_cookie, r, NULL);
         for (t1 = t2 = time(NULL); stop(p_cookie) == false; t2 = time(NULL)) {
+                struct mdns_hdr ahdr = {0};
                 struct rr_entry *entries;
 
                 if (difftime(t2, t1) >= (double) interval) {
-                        if ((r = mdns_send(ctx, RR_PTR, name)) < 0) {
+                        if ((r = mdns_send(ctx, &hdr, &qn)) < 0) {
                                 callback(p_cookie, r, NULL);
                                 continue;
                         }
                         t1 = t2;
                 }
-                r = mdns_recv(ctx, &entries);
+                r = mdns_recv(ctx, &ahdr, &entries);
                 if (r == MDNS_NETERR && os_wouldblock())
+                        continue;
+
+                if (ahdr.num_ans_rr + ahdr.num_add_rr == 0)
                         continue;
 
                 for (struct rr_entry *entry = entries; entry; entry = entry->next) {

828412e6c93bf0722fd2a55b67f1d890858998a3
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2016-03-24 18:30:08 +0100

    Add missing include "config.h"

--- compat/compat.c
+++ compat/compat.c
@@ -16,6 +16,10 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
 #include <assert.h>
 #include <errno.h>
 #include <string.h>
--- src/rr.c
+++ src/rr.c
@@ -24,6 +24,10 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
